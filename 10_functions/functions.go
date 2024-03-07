8f62857c092c8123489634315cce514a611692b2
Antonis Mavrakis <antonis.mavrak@gmail.com>
2024-03-07 17:04:21 +0200

    functions

---
+++ 10_functions/functions.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	x, y := 5, 6
+	fmt.Println(add(x, y))
+
+	num := 5
+	fmt.Println(factorial(num))
+}
+
+func add(num1 int, num2 int) int {
+	return num1 + num2
+}
+
+func factorial(num int) int {
+	if num == 0 {
+		return 1
+	}
+	return num * factorial(num-1)
+}
